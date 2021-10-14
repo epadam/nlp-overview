ac42436d42df9559d6bec9a81d6464f0baf603e9
Andrew Huang (Cheng Chiang) <33182390+epadam@users.noreply.github.com>
2021-10-14 08:38:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 if 'input' not in st.session_state:
-	st.session_state.input = None
+	st.session_state.input = Hi
 
 if 'option' not in st.session_state:
 	st.session_state.option = 0
@@ -13,12 +13,12 @@ st.title('Chatbot Test')
 URL = 'http://35.236.187.220:8080/webhooks/rest/webhook'
 
 
-input = st.text_input('Type your message here', 'Hi')
+input = st.text_input('Type your message here', st.session_state.input)
 
-if st.session_state.input is None:
-	st.session_state.input = input
+#if st.session_state.input is None:
+#	st.session_state.input = input
 
-response = requests.post(URL, json={"sender":"Andrew", "message":st.session_state.input})
+response = requests.post(URL, json={"sender":"Andrew", "message":input})
 
 st.session_state.input = None
 
@@ -37,7 +37,7 @@ if response is not None:
       for button in res['buttons']:
         options.append(button['title'])        
       option = st.radio('', options , key='option')
-      #st.session_state.input = res['buttons'][options.index(option)]['payload']
+      st.session_state.input = res['buttons'][options.index(option)]['payload']
 
      
 
