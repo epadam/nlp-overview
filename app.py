6b76221df1a5a58c408d8b238674a651b655e233
Andrew Huang (Cheng Chiang) <33182390+epadam@users.noreply.github.com>
2021-10-14 06:30:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,11 @@ URL = 'http://35.236.187.220:8080/webhooks/rest/webhook'
 
 input = st.text_input('Type your message here', 'Hi')
 
-response = requests.post(URL, json={"sender":"Andrew", "message":input})
+def call_rasa(msg):
+  return response = requests.post(URL, json={"sender":"Andrew", "message":msg})
+
+response = call_rasa(input)
+
 st.subheader('Rasa Response')
 if response is not None:
   json_response = response.json()
@@ -19,9 +23,11 @@ if response is not None:
       st.image(res['image'])
     if 'buttons' in res:
       options =[]
-      for idx,  button in enumerate(res['buttons']):
+      for button in res['buttons']:
         options.append(button['title'])        
       option = st.radio('', options)
-      #response = requests.post(URL, json={"sender":"Andrew", "message":res['buttons'][idx]['payload']})
+      #input = res['buttons'][options.index(option)]['payload']
+      #response = call_rasa(input)
+
 
   
