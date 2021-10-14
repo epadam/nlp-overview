1d02d2dacceabe4afe414bea34dbbe008c97a8cd
Andrew Huang (Cheng Chiang) <33182390+epadam@users.noreply.github.com>
2021-10-14 02:40:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ URL = 'http://35.236.187.220:8080/webhooks/rest/webhook'
 input = st.text_input('input')
 
 response = requests.post(URL, data={'sender':'Andrew', 'message':'Hello'})
-if response is not NONE:
+if response is not None:
   json_response = response.json()
   st.write('Response')
   st.text(json_response['text'])
