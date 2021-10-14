d8e9dfba34974e6a78c7693fc44a7dc00c59cce8
Andrew Huang (Cheng Chiang) <33182390+epadam@users.noreply.github.com>
2021-10-14 07:14:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import streamlit as st
 import requests
 
+if 'input' not in st.session_state:
+	st.session_state.input = 'Hi'
+
 
 st.title('Chatbot Test')
 
 URL = 'http://35.236.187.220:8080/webhooks/rest/webhook'
 
 input = st.text_input('Type your message here', 'Hi', key="input")
+
+
 
 def call_rasa(msg):
   response = requests.post(URL, json={"sender":"Andrew", "message":msg})
@@ -27,7 +32,9 @@ if response is not None:
       for button in res['buttons']:
         options.append(button['title'])        
       option = st.radio('', options)
-      input = res['buttons'][options.index(option)]['payload']
+      st.session_state.input = res['buttons'][options.index(option)]['payload']
+
+      #input = res['buttons'][options.index(option)]['payload']
 
 
 
