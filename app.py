b047ccd54e7b5f9e605ad83f2ac9832c86f1cdb8
Andrew Huang (Cheng Chiang) <33182390+epadam@users.noreply.github.com>
2021-10-14 02:23:23 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import requests
+
+
+st.title('Chatbot Test')
+
+URL = 'http://35.236.187.220:8080/webhooks/rest/webhook'
+
+input = st.text_input('input')
+
+response = requests.post(URL, data={'sender':'Andrew', 'message':'Hello'})
+
+json_response = response.json()
+
+st.write('Response')
+st.text(json_response['text'])
